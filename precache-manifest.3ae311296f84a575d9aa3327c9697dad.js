d949b443475e687f4e27c91a0626f230f1c01dcc
ggaswint <ggaswint@uci.edu>
2020-09-09 14:14:52 -0700

    Updates

---
+++ precache-manifest.3ae311296f84a575d9aa3327c9697dad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fba99d2c5bc0070643b666b4f277643",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6503d566670debbbf223",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "14a61ad853248794c46c",
+    "url": "/portfolio/static/js/2.1e0e80a5.chunk.js"
+  },
+  {
+    "revision": "69211abf3b49d4ff08de8fc1299580b7",
+    "url": "/portfolio/static/js/2.1e0e80a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6503d566670debbbf223",
+    "url": "/portfolio/static/js/main.71d94196.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "01094f5ce35dc87ea79a2ba3848e17b0",
+    "url": "/portfolio/static/media/NuReco.01094f5c.pdf"
+  },
+  {
+    "revision": "adb1f038d74337b09f21c940aeff5ba1",
+    "url": "/portfolio/static/media/resume.adb1f038.pdf"
+  }
+]);
